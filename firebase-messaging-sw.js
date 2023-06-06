848a2f1931a12eb502faaa5e8411e4dd4d61f66e
Hussein Al Mawla <info@hmawla.com>
2023-06-06 15:10:51 +0300

    adventure

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-messaging.js');
+const firebaseConfig = {
+    apiKey: "AIzaSyBUCSAxNMbgZGGdoiHia2bNC_BcTnyDxiM",
+    authDomain: "yumna-website.firebaseapp.com",
+    projectId: "yumna-website",
+    storageBucket: "yumna-website.appspot.com",
+    messagingSenderId: "293455560102",
+    appId: "1:293455560102:web:03f93a48809c5d7cbfe367",
+    measurementId: "G-R96T2NY7SR"
+};
+firebase.initializeApp(firebaseConfig);
+
+self.addEventListener('notificationclick', event => {
+    console.log(event)
+});
+
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+    };
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
+
